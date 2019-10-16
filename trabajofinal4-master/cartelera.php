ff247a26ab977d44763615ccf64089df4cc0eb8c
Bianca Pilegi <46908195+bpilegi98@users.noreply.github.com>
2019-10-16 20:43:48 -0300

    TP Final - 01
    Separación por partes, cambios por arriba de cosas

---
+++ trabajofinal4-master/cartelera.php
@@ -0,0 +1,95 @@
+<?php
+ include "header.html";
+ ?>
+
+<html>
+<!-- Cartelera -->
+<body>
+<div id="about" class="page-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-heading">
+                    <h4>Películas en cartelera</h4>
+                    <div class="line-dec"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="service-item first-service">
+                    <div class="icon"></div>
+                    <h4>Easy Customizations</h4>
+                    <p>Meteor is free HTML website template by Tooplate. Feel free to use this layout for your
+                        project.</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="service-item second-service">
+                    <div class="icon"></div>
+                    <h4>Creative Ideas</h4>
+                    <p>Biodiesel schltz suculents phone cliche ramps snackwave coloring book tumeric poke,
+                        typewriter.</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="service-item third-service">
+                    <div class="icon"></div>
+                    <h4>Good Profit</h4>
+                    <p>Biodiesel schltz suculents phone cliche ramps snackwave coloring book tumeric poke,
+                        typewriter.</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="service-item fourth-service">
+                    <div class="icon"></div>
+                    <h4>Open To Public</h4>
+                    <p>Biodiesel schltz suculents phone cliche ramps snackwave coloring book tumeric poke,
+                        typewriter.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="what-we-do">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="left-text">
+                    <h4>New offers just arrived,<br>Get it now and take profit.</h4>
+                    <p>Gochujang pitchfork post-ironic hammock kombucha fanny pack cronut. Raw denim chicharrones
+                        semiotics af truffaut pinterest. Distillery letterpress authentic adaptogen. Meditation
+                        schlitz humblebrag photo booth celiac cliche chartreuse.<br><br>Pinterest 90's wolf celiac
+                        dreamcatcher listicle deep v semiotics. Intelligentsia literally meggings trust fund put a
+                        bird on it. Shoreditch crucifix artisan pug shaman twee. Health goth bicycle rights retro
+                        iPhone.</p>
+                    <ul>
+                        <li>
+                            <div class="white-button">
+                                <a href="#" class="scroll-link" data-id="portfolio">Discover More</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="primary-button">
+                                <a href="#">Purchase Now</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="right-image">
+                    <img src="img/what-we-do-right-image.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+<?php
+ include "footer.html";
+ ?>
+
+</html>
---
+++ trabajofinal4-master/formulario.php
@@ -0,0 +1,75 @@
+<?php
+ include "header.html";
+ ?>
+
+<html>
+
+<!-- FORMULARIO -->
+<body>
+<div id="contact" class="page-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-heading">
+                    <h4>Sign up</h4>
+                    <div class="line-dec"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="map">
+                    <img src="img/map.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <form id="contact" action="" method="post">
+                        <div class="col-md-6">
+                            <fieldset>
+                                <input name="name" type="text" class="form-control" id="name"
+                                    placeholder="Nombre" required>
+                            </fieldset>
+                        </div>
+                        <div class="col-md-6">
+                            <fieldset>
+                                <input name="surname" type="text" class="form-control" id="surname"
+                                    placeholder="Apellido" required>
+                            </fieldset>
+                        </div>
+                        <div class="col-md-6">
+                            <fieldset>
+                                <input name="email" type="email" class="form-control" id="email"
+                                    placeholder="Email" required>
+                            </fieldset>
+                        </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <input name="nickname" type="text" class="form-control" id="nickname"
+                                    placeholder="Usuario" required>
+                            </fieldset>
+                        </div>
+                        <div class="col-md-6">
+                            <fieldset>
+                                <input name="password" type="password" class="form-control" id="password"
+                                    placeholder="Contraseña" required>
+                            </fieldset>
+                        </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <button type="submit" id="form-submit" class="btn">Registrarse</button>
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+<?php
+ include "footer.html";
+ ?>
+</html>
